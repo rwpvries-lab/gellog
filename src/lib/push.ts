0b6ecb5d4c895ef4e39e428be070128c9245c075
Ruben de Vries <ruvries@icloud.com>
2026-03-29 17:44:17 +0200

    Bug fix 5: Salon analytics, push notification groundwork

--- src/lib/push.ts
+++ src/lib/push.ts
@@ -66,9 +66,14 @@ export async function deletePushSubscription(): Promise<void> {
   }
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+  const buf = new ArrayBuffer(rawData.length);
+  const outputArray = new Uint8Array(buf);
+  for (let i = 0; i < rawData.length; i++) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
